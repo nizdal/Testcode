3834844ecf411f5d64e0917bab3aea5771f59efe
shaik shaguftha <shaik shaguftha@LAPTOP-VIU2MRF9>
2018-10-15 12:12:50 +0530

    1stpush

---
+++ MagentoTest/src/main/java/Logout.java
@@ -0,0 +1,22 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Logout {
+
+	WebDriver driver;
+	
+	By logout = By.id("Logout");
+			
+	public Logout(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	public void clickOnLogout()
+	{
+		driver.findElement(logout).click();
+	}
+	
+}
---
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -0,0 +1,39 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class MagentoTest {
+
+	@Test
+	public void posCredentials()throws Exception {
+		
+	String url = "http://www.magento.com";
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get(url);
+	
+	Main m = new Main(driver);
+	m.clickOnMyacc();
+	
+	
+	Login login = new Login(driver);
+	login.clickOnLogin();
+	login.typeEmail("subramanyaraj87@gmail.com");
+	login.typePass("Welcome123");
+	
+	
+	Logout logout = new Logout(driver);
+	logout.clickOnLogout();
+	
+	driver.quit();
+
+	}
+
+}
